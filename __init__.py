9c7038e8cb96d5dba872cd72296b9fccef054bd3
danpetruk <danpetruk@gmail.com>
2025-05-01 03:02:41 +0300

    token support

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
-from .auth import Auth
-from .data_loader import DataLoader
+from auth import Auth
+from data_loader import DataLoader
 
-def load(exchange, market , load_type, symbols, start_date, end_date, username, password):
-    auth: Auth = Auth(base_url="http://194.87.140.9:30080")
+
+def get_loader(token) -> DataLoader:
+    auth: Auth = Auth(base_url="http://194.87.140.9:30080")  # todo: check if lib version is depreciated
+    if len(token.split('-')) < 2:
+        raise Exception("Invalid token")
+    username = "-".join(token.split("-")[:-1])
+    password = token.split("-")[-1]
     auth.login(username, password)
-
     loader = DataLoader(auth)
-    loader.load_data(exchange, market , load_type, symbols, start_date, end_date)
+    return loader
+
+
